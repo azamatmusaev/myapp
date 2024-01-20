710ed286b5249ffb13aecbc8de85d86c5998c9e1
azamatmusaev <@example.com>
2024-01-20 14:36:48 +0600

    add project

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+import { useState } from 'react';
+import Form from './componets/form/Form';
+import List from './componets/list/List';
+
+function App() {
+  const [texts, setTexts] = useState([]);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Form texts={texts} setTexts={setTexts} />
+        <List texts={texts} />
+      </header>
+    </div>
+  );
+}
+
+export default App;
